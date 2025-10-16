7e20995f55aae5a11a327348bfa201090e0e14f8
vestiapani <ivan131335@gmail.com>
2025-10-16 20:22:48 +0700

    Menambahkan NPM

--- Prak_Pertama/Ivan.java
+++ Prak_Pertama/Ivan.java
@@ -2,5 +2,6 @@ public class Ivan{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Ivan Wirahadi Permana");
+        System.out.println("4524210045");
     }
 }
